d2a3dc00738f3adb0dc585b5e17f1053c0b501d5
mivanoski <44038646+mivanoski@users.noreply.github.com>
2022-04-20 18:00:11 +0200

    Init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Yiimp Server Setup
+
+Installation files for Yiimp Server Installer
+
+#### These files do nothing on their own please go to https://github.com/mivanoski/Pool4U_Yiimp_Installer
---
+++ menu.sh
@@ -0,0 +1,30 @@
+################################################################################
+# Source https://mailinabox.email/ https://github.com/mail-in-a-box/mailinabox #
+# Updated by Pool4U for YiiMP use...                                           #
+# This script is intended to be ran from the Yiimp Server Installer            #
+################################################################################
+
+source /etc/functions.sh
+
+RESULT=$(dialog --stdout --nocancel --default-item 1 --title "Pool4U Yiimp Server Installer v1.0" --menu "Choose one" -1 60 16 \
+' ' "- YiiMP Server Install -" \
+1 "YiiMP Single Server" \
+' ' "- More Will Be Added Later -" \
+2 Exit)
+if [ $RESULT = ]
+then
+bash $(basename $0) && exit;
+fi
+
+if [ $RESULT = 1 ]
+then
+clear;
+cd $HOME/yiimpserver/install
+source bootstrap_single.sh;
+fi
+
+if [ $RESULT = 2 ]
+then
+clear;
+exit;
+fi
